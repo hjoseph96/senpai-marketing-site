d8bfb4f190f7f161232183a1ae0e80582c23c9f6
kh <kzar1102@gmail.com>
2023-12-15 12:43:04 -0500

    Revert "init commit"
    This reverts commit 977ff4dcdea7230e3f926089a21ff6c3cbec2fde.

--- src/components/center-title.tsx
+++
@@ -1,7 +0,0 @@
-export default function CenterTitle({title, whiteMode = false, className}: {title: string; whiteMode?: boolean; className?: string }) {
-  return (
-      <div className="home-title text-center">
-        <h2 className={( className ?? "h2 pb-4 text-primary") + ' home-title-heading uppercase inline-block' + (whiteMode ? " white-mode" : "")}>{title}</h2>
-      </div>
-    );
-}
